f6688be4b92271bd42349bff5663f71c10e12451
RGDLucky <ryan.dinville@gmail.com>
2023-12-06 17:47:38 -0700

    added random feature that ranomizes all elements when the randomize button is pressed

--- script.js
+++ script.js
@@ -1,9 +1,21 @@
 let elements = document.getElementsByClassName(".element");
+let randomButton = document.getElementById("random-button");
+
+random();
+// Event Listeners
+randomButton.addEventListener("click", () => random());
 
 // Randomize elements in array
-Array.from(document.querySelectorAll(".element")).forEach(function (element) {
-  let new_height = Math.random() * 100;
-  element.style.height = new_height + "%";
-  let new_top = 100 - new_height;
-  element.style.bottom = "-" + new_top + "%";
-});
+function random() {
+  Array.from(document.querySelectorAll(".element")).forEach(function (element) {
+    let new_height = Math.random() * 100;
+    element.style.height = new_height + "%";
+    let new_top = 100 - new_height;
+    element.style.bottom = "-" + new_top + "%";
+  });
+}
+
+// Sorting
+
+// Insertion Sort
+function insertionSort() {}
