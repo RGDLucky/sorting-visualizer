67872eb795e5e345df6ee73c75975b301cddb5ff
RGDLucky <ryan.dinville@gmail.com>
2023-12-11 23:17:21 -0700

    Added insertion sort but having toubles moving the elements when swapping

--- script.js
+++ script.js
@@ -14,18 +14,19 @@ enterButton.addEventListener("click", () => {
 
 sortButton.addEventListener("click", () => {
   let selected = document.querySelector('input[type="radio"]:checked');
+  let elementArray = Array.from(document.querySelectorAll(".element"));
   switch (selected.value) {
     case "insertion":
-      insertionSort();
+      insertionSort(elementArray);
       break;
     case "merge":
-      mergeSort();
+      mergeSort(elementArray);
       break;
     case "quick":
-      quickSort();
+      quickSort(elementArray);
       break;
     case "selection":
-      selectionSort();
+      selectionSort(elementArray);
       break;
     default:
       console.log("Something went wrong with the radio buttons");
@@ -45,21 +46,39 @@ function random() {
 // Sorting
 
 // Insertion Sort
-function insertionSort() {
+function insertionSort(arr) {
   console.log("insertion sort");
+  for (let i = 1; i < arr.length - 1; i++) {
+    //console.log(arr.length);
+    for (let j = i - 1; j >= 0; j--) {
+      let element1 = arr[j].style.height.substring(0, arr[j].length);
+      let element2 = arr[j + 1].style.height.substring(0, arr[j + 1].length);
+      // console.log(element1);
+      if (parseFloat(element1) < parseFloat(element2)) {
+        break;
+      }
+      //let distance = 100 / arr.length;
+      //console.log(distance);
+      arr[j].style.transform = "translateX(100%)";
+      arr[j + 1].style.transform = "translateX(-100%)";
+      let temp = arr[j];
+      arr[j] = arr[j + 1];
+      arr[j + 1] = temp;
+    }
+  }
 }
 
 // Selection Sort
-function selectionSort() {
+function selectionSort(arr) {
   console.log("selection sort");
 }
 
 // Merge sort
-function mergeSort() {
+function mergeSort(arr) {
   console.log("merge sort");
 }
 
 // Quick sort
-function quickSort() {
+function quickSort(arr) {
   console.log("quick sort");
 }
