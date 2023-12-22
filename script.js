7a80e5e1ce761cbceef1e33ff298683b7eed8964
RGDLucky <ryan.dinville@gmail.com>
2023-12-22 12:34:46 -0700

    tried grid need to figure out how to make them all on the bottom row

--- script.js
+++ script.js
@@ -35,11 +35,17 @@ sortButton.addEventListener("click", () => {
 
 // Randomize elements in array
 function random() {
+  let count = 0;
   Array.from(document.querySelectorAll(".element")).forEach(function (element) {
     let new_height = Math.random() * 100;
     element.style.height = new_height + "%";
     let new_top = 100 - new_height;
     element.style.bottom = "-" + new_top + "%";
+    //element.style.bottom = 0;
+    //element.style.top = "100%";
+    let move = count * 100;
+    element.style.transform = "translateX(" + move + "%)";
+    count++;
   });
 }
 
